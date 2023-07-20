53b4e319f132871e3347b3f564ebf38800e2e256
asalche <90353796+Asalnjf@users.noreply.github.com>
2023-07-20 14:57:41 +0200

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x1fdc10612e4293090e6b444dcab008118bcdeb5c";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
